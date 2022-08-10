f577d2ef10d268006ee0d3122f5966c2174c5741
OliviavP <livalossy@gmail.com>
2022-08-09 21:26:33 -0600

    Finished

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,97 @@
 import React from 'react'
+import axios from 'axios'
+import Form from './Form'
+import TodoList from './TodoList'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+    error: '',
+    todoInput: '',
+    displayComp: true,
+  }
+
+  todoChange = (e) => {
+    const { value } = e.target
+    this.setState({ ...this.state, todoInput: value })
+  }
+
+  todoError = (err) => {
+    this.setState({ ...this.state, error: err.responee.data.message })
+  }
+
+  resetForm = () => {
+    this.setState({ ...this.state, todoInput: '' })
+  }
+
+  newTodo = () => {
+    axios
+      .post(URL, { name: this.sstate.todoInput })
+      .then((res) => {
+        this.setState({
+          ...this.state,
+          todos: this.state.todos.concat(res.data.data),
+        })
+        this.resetForm()
+      })
+      .catch(this.todoError)
+  }
+
+  todoSubmit = (e) => {
+    e.preventDefault()
+    this.newTodo()
+  }
+
+  fetchTodos = () => {
+    axios
+      .get(URL)
+      .then((res) => {
+        this.setState({ ...this.state, todos: res.data.data })
+      })
+      .catch(this.todoError)
+  }
+
+  toggleComplete = (id) => () => {
+    axios
+      .patch(`${URL}/${id}`)
+      .then((res) => {
+        this.setState({
+          ...this.state,
+          todos: this.state.todos.map((td) => {
+            if (td.id !== id) return td
+            return res.data.data
+          }),
+        })
+      })
+      .catch(this.todoError)
+  }
+
+  toggleDisComp = () => {
+    this.setState({ ...this.state, displayComp: !this.state.displayComp })
+  }
+
+  componentDidMount() {
+    this.fetchTodos()
+  }
   render() {
-    return null
+    return (
+      <div>
+        <div id='error'>Error: {this.state.error}</div>
+        <TodoList
+          todos={this.state.todos}
+          displayComp={this.state.displayComp}
+          toggleComplete={this.toggleComplete}
+        />
+        <Form
+          todoSubmit={this.todoSubmit}
+          toggleDisComp={this.toggleDisComp}
+          todoChange={this.todoChange}
+          todoInput={this.state.todoInput}
+          displayComp={this.state.displayComp}
+        />
+      </div>
+    )
   }
 }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default class Form extends React.Component {
   render() {
-    return null
+    return (
+      <form id='form' onSubmit={this.props.todoSubmit}>
+        <input
+          value={this.props.todoInput}
+          type='text'
+          placeholder='Type new todo'
+          onChange={this.props.todoChange}
+        ></input>
+        <input type='submit'></input>
+        <button onClick={this.props.toggleDisComp}>
+          {this.props.displayComp ? 'Hide' : 'Show'}
+          Completed
+        </button>
+      </form>
+    )
   }
 }
